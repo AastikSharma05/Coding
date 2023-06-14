90fdf3bc37ac4e1ac4ca8f00f1553fa79a17d3b5
Aastik Sharma <84850243+AastikSharma05@users.noreply.github.com>
2023-06-14 22:13:05 +0530

    Update RotateArray.cpp

--- Array/RotateArray.cpp
+++ Array/RotateArray.cpp
@@ -1,4 +1,3 @@
-```
 Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
 
 
@@ -10,7 +9,7 @@ arr[] = {1,2,3,4,5}
 Output: 3 4 5 1 2
 Explanation: 1 2 3 4 5  when rotated
 by 2 elements, it becomes 3 4 5 1 2.
-```
+
 
 Code:
 
